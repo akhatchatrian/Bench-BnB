b3bdf824ee4acec1515b54469f184119915b62d4
Arman Khatchatrian <armank0089@gmail.com>
2020-04-24 15:12:41 -0700

    add bench backend

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,19 @@
+class Api::BenchesController < ApplicationController
+
+    def index 
+        @benches = Bench.all 
+        render :index
+    end
+
+    def create
+        @bench = Bench.create(bench_params)
+        render :show
+    end
+
+    private
+
+    def bench_params
+        params.require(:bench).permit(:description, :lat, :lng)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { formant: :json } do
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create]
+    resources :benches, only: [:create, :index]
   end
 
   root to: "static_pages#root"
